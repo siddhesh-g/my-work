d77ed2a0fcbb5f965126c0e12edbc8d1c87ff8c2
siddhesh-g <siddheshghargane2022@gmail.com>
2024-12-23 23:46:03 +0530

    Add files via upload
    this is python program of password login . password can be alphanumeric or any. when entered password will be correct then file can be access

---
+++ password login.py
@@ -0,0 +1,16 @@
+class Password:
+    def __init__(self, username,password):
+        self.username = username
+        self.password = password
+    def setpassword(Password,sername,password):
+        password = password
+        sername = sername
+        if sername== Password.username and password == Password.password:
+            print('login successfully ')
+            file=open("translation.py",'r')
+            content=file.read()
+            print(content)
+        else:
+            print('enter again')
+passw=Password('siddhesh','siddhesh1103')
+passw.setpassword(str(input('enter your username: ')),str(input('enter password: ')))
